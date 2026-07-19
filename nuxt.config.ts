ad089097a92056ed295242ced1ab28da7002a269
Igor Zabotin <igrzbtn@gmail.com>
2026-07-19 12:00:20 +0500

    chore: настройка Nuxt
    - Добавлен линтер.
    - Настроен CI.yml: lint, typecheck.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/ui'
+    "@nuxt/eslint",
+    "@nuxt/ui"
   ],
 
   devtools: {
     enabled: true
   },
 
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
   routeRules: {
-    '/': { prerender: true }
+    "/": { prerender: true }
   },
 
-  compatibilityDate: '2026-06-30',
+  compatibilityDate: "2026-06-30",
 
   eslint: {
     config: {
       stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
+        indent: 2,
+        quotes: "double",
+        commaDangle: "never",
+        braceStyle: "1tbs"
       }
     }
   }
